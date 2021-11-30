ce58fa81e227dfe3454f6d91b9f00989d975d083
gztjzxl3 <94006598+gztjzxl3@users.noreply.github.com>
2021-11-30 20:56:26 +0800

    Create big_to_small.py

---
+++ for_excel/big_to_small.py
@@ -0,0 +1,20 @@
+def f(x, small_base):
+    quotient, remainder = x // small_base, x % small_base
+    return quotient, remainder
+
+
+def transform(x, small_base):
+    rd_li = []
+    qt, rd = f(x, small_base)
+    rd_li.append(str(rd))
+    while qt != 0:
+        qt, rd = f(qt, small_base)
+        rd_li.append(str(rd))
+    res = ''.join(rd_li[::-1])
+    return res
+
+
+if __name__ == '__main__':
+    small_base = 2
+    res = transform(8, 2)
+    print(res)

085821de720a1e4ea0a43d294ffc236a67958d49
gztjzxl3 <94006598+gztjzxl3@users.noreply.github.com>
2021-11-30 21:11:38 +0800

    Update big_to_small.py

--- for_excel/big_to_small.py
+++ for_excel/big_to_small.py
@@ -1,21 +1,22 @@
-# These are only for decimal to smaller base
-def f(x, small_base):
-    quotient, remainder = x // small_base, x % small_base
+# These are only for decimal to other bases' system
+
+def f(x, base):
+    quotient, remainder = x // base, x % base
     return quotient, remainder
 
 
-def transform(x, small_base):
+def transform(x, base):
     rd_li = []
-    qt, rd = f(x, small_base)
+    qt, rd = f(x, base)
     rd_li.append(str(rd))
     while qt != 0:
-        qt, rd = f(qt, small_base)
+        qt, rd = f(qt, base)
         rd_li.append(str(rd))
     res = ''.join(rd_li[::-1])
     return res
 
 
 if __name__ == '__main__':
-    small_base = 2
+    base = 2
     res = transform(8, 2)
     print(res)
